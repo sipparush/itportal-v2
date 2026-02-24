350efdfee3a36a1c244780a868aa4de88178827b
sipparush <sipparush@jventures.co.th>
2026-02-24 17:55:46 +0700

    add aws functions

--- src/app/operations/page.js
+++ src/app/operations/page.js
@@ -105,6 +105,13 @@ export default function OperationsPage() {
                                                 <span className="font-medium">Create EC2 Instance</span>
                                             </a>
                                         </li>
+                                        <li>
+                                            <a href="https://api-monitor.jventures.co.th/" target="_blank" rel="noopener noreferrer" className="flex items-center text-gray-700 hover:text-red-600 transition-colors group">
+                                                <svg className="w-5 h-5 mr-3 text-gray-400 group-hover:text-red-500" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"></path></svg>
+                                                <span className="font-medium">Monitor API</span>
+                                            </a>
+                                        </li>
+
                                     </ul>
                                 </div>
                             </div>
